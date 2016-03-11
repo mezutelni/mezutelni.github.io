330062ef1fe250830a52e87a0c0f2c3f669cd85d
mezutelni <mezutelni@gmail.com>
2016-03-11 17:28:17 +0100

    Added

---
+++ script/konami.js
@@ -0,0 +1,44 @@
+(function ( $ ) {
+	"use strict";
+
+	$.fn.konami = function( options ) {
+		var opts, controllerCode;
+
+		opts = $.extend({}, $.fn.konami.defaults, options);
+		controllerCode = [];
+
+		// Note that we use the passed-in options, not the resolved options.
+		opts.eventProperties = $.extend({}, options,  opts.eventProperties);
+
+		this.keyup(function( evt ) {
+			var code = evt.keyCode || evt.which;
+
+			if ( opts.code.length > controllerCode.push( code ) ) {
+				return;
+			}
+
+			if ( opts.code.length < controllerCode.length ) {
+				controllerCode.shift();
+			}
+
+			if ( opts.code.toString() !== controllerCode.toString() ) {
+				return;
+			}
+
+			opts.cheat(evt, opts);
+
+		});
+
+		return this;
+	};
+
+	$.fn.konami.defaults = {
+		code : [38,38,40,40,37,39,37,39,66,65],
+		eventName : 'konami',
+		eventProperties : null,
+		cheat: function(evt, opts) {
+			$(evt.target).trigger(opts.eventName, [ opts.eventProperties ]);
+		}
+	};
+
+})( jQuery );
---
+++ script/script.js
@@ -0,0 +1,55 @@
+        $(document).ready(
+    function() {
+        $("#tilesmall1").click(function() {
+            $("#whitebox1").fadeToggle();
+        });
+         $("#tilesmall1").click(function() {
+            $("#textbox1").fadeToggle();
+        });
+    });
+  $(document).ready(
+function() {
+        $("#tilesmall2").click(function() {
+            $("#whitebox2").fadeToggle();
+        });
+         $("#tilesmall2").click(function() {
+            $("#textbox2").fadeToggle();
+        });
+    });
+  $(document).ready(
+function() {
+        $("#tilesmall3").click(function() {
+            $("#whitebox3").fadeToggle();
+        });
+         $("#tilesmall3").click(function() {
+            $("#textbox3").fadeToggle();
+        });
+    });
+  $(document).ready(
+function() {
+        $("#tilesmall4").click(function() {
+            $("#whitebox4").fadeToggle();
+        });
+         $("#tilesmall4").click(function() {
+            $("#textbox4").fadeToggle();
+        });
+    });
+  $(document).ready(
+function() {
+        $("#tilesmall5").click(function() {
+            $("#whitebox5").fadeToggle();
+        });
+         $("#tilesmall5").click(function() {
+            $("#textbox5").fadeToggle();
+        });
+    });
+  $(document).ready(
+function() {
+        $("#tilesmall6").click(function() {
+            $("#whitebox6").fadeToggle();
+        });
+         $("#tilesmall6").click(function() {
+            $("#textbox6").fadeToggle();
+        });
+    });
+
